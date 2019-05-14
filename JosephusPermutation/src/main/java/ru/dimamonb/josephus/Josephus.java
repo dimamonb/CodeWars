30d1fee4eec84f9558ee46e87190da43b47bd8e1
d.onatskiy <dimaon@gmail.com>
2019-05-14 13:43:27 +0300

    add public static void main

--- JosephusPermutation/src/main/java/ru/dimamonb/josephus/Josephus.java
+++ JosephusPermutation/src/main/java/ru/dimamonb/josephus/Josephus.java
@@ -1,6 +1,7 @@
 package ru.dimamonb.josephus;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Josephus {
@@ -8,5 +9,9 @@ public class Josephus {
         List<T> result = new ArrayList<>();
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        josephusPermutation(new ArrayList(Arrays.asList(1,2,3,4,5,6,7)),3);
     }
 }
