5eded0f6947dafca4e4bcc38c0ac935732990e42
d.onatskiy <dimaon@gmail.com>
2019-05-15 14:17:36 +0300

    finished

--- JosephusPermutation/src/main/java/ru/dimamonb/josephus/Josephus.java
+++ JosephusPermutation/src/main/java/ru/dimamonb/josephus/Josephus.java
@@ -6,21 +6,27 @@ import java.util.List;
 
 
 public class Josephus {
-    public static int joseph(int size, int k){
-        return size>1 ? (joseph (size-1, k) + k - 1) % size + 1 : 1;
+
+    public static int joseph(int size, int k) {
+        return size > 1 ? (joseph(size - 1, k) + k - 1) % size + 1 : 1;
     }
+
+    public static int joseph1(int size, int k) {
+        return size > 1 ? (joseph(size - 1, k) + k - 1) % size + 1 : 1;
+    }
+
     public static <T> List<T> josephusPermutation(final List<T> items, final int k) {
         List<T> result = new ArrayList<>();
         int idx = 0;
         while (!items.isEmpty()) {
-
-            idx = joseph(items.size(), k);
+            idx = (idx + k - 1)%items.size();
+            result.add(items.get(idx));
+            items.remove(idx);
         }
-
         return result;
     }
 
     public static void main(String[] args) {
-        josephusPermutation(new ArrayList(Arrays.asList(1, 2, 3, 4, 5, 6, 7)), 3);
+        josephusPermutation(new ArrayList(Arrays.asList()), 3);
     }
 }
